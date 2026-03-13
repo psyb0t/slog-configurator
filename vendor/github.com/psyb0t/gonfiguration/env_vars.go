ff0a4ddc835e17f195304dd05f1dd3113bfcbfd4
Ciprian Mandache <psyb0t@51k.eu>
2026-03-13 23:03:33 +0200

    fuckin env*

---
+++ vendor/github.com/psyb0t/gonfiguration/env_vars.go
@@ -0,0 +1,55 @@
+package gonfiguration
+
+import (
+	"fmt"
+	"maps"
+	"os"
+	"strings"
+)
+
+const (
+	envVarNumParts = 2
+)
+
+func GetEnvVars() map[string]string {
+	return gonfig.getEnvVars()
+}
+
+func (g *gonfiguration) setEnvVar(key, val string) {
+	g.Lock()
+	defer g.Unlock()
+
+	g.envVars[key] = val
+}
+
+func (g *gonfiguration) setEnvVars(envVars map[string]string) {
+	for key, val := range envVars {
+		g.setEnvVar(key, val)
+	}
+}
+
+func (g *gonfiguration) getEnvVars() map[string]string {
+	g.RLock()
+	defer g.RUnlock()
+
+	envVarsCopy := make(map[string]string, len(g.envVars))
+	maps.Copy(envVarsCopy, g.envVars)
+
+	return envVarsCopy
+}
+
+func getEnvVars() (map[string]string, error) {
+	envVars := map[string]string{}
+	rawVars := os.Environ()
+
+	for _, rawVar := range rawVars {
+		parts := strings.SplitN(rawVar, "=", envVarNumParts)
+		if len(parts) != envVarNumParts {
+			return nil, fmt.Errorf("%s: %w", rawVar, ErrInvalidEnvVar)
+		}
+
+		envVars[parts[0]] = parts[1]
+	}
+
+	return envVars, nil
+}
